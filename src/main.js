d2b5d0d55065b363f2671771a5b91d0f28d2ba43
Polo <431824582@qq.com>
2021-04-24 23:37:34 +0800

    项目初始化-分装简易axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
-
-// 加载全局样式文件
 import './styles/index.less'
+
+Vue.use(ElementUI)
+// 加载全局样式文件
 Vue.config.productionTip = false
 
 new Vue({
